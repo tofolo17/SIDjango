dc41e60ad1ed6b0ff3a02c22477206184191762b
gusta <valenciotofolo@usp.br>
2021-06-18 12:26:02 -0300

    Login padrão implementado.

--- ic/professor/urls.py
+++ ic/professor/urls.py
@@ -1,5 +1,8 @@
-app_name = 'professor'
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
-
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard')
 ]
