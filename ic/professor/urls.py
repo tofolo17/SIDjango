87ad8f046df18d192502da4494e836a7bc4c9c7d
gusta <valenciotofolo@usp.br>
2021-07-10 16:03:14 -0300

    Frontend de registration finalizado.

--- ic/professor/urls.py
+++ ic/professor/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
 
     path('account/', views.SimulatorListView.as_view(), name='dashboard'),
     path('account/create/', views.SimulatorCreateView.as_view(), name='create'),
-    path('account/<int:pk>', views.SimulatorDetailView.as_view(), name='detail'),
-    path('account/<int:pk>/update', views.SimulatorUpdateView.as_view(), name='update'),
-    path('account/<int:pk>/delete', views.SimulatorDeleteView.as_view(), name='delete'),
-    path('account/<int:pk>/update_token', views.update_token, name='change_token'),
+    path('account/<int:pk>/', views.SimulatorDetailView.as_view(), name='detail'),
+    path('account/<int:pk>/update/', views.SimulatorUpdateView.as_view(), name='update'),
+    path('account/<int:pk>/delete/', views.SimulatorDeleteView.as_view(), name='delete'),
+    path('account/<int:pk>/update_token/', views.update_token, name='change_token'),
 
     path('simulator/explore', views.ExploreSimulatorListView.as_view(), name='explore'),
     path('simulator/private/<str:token>', views.access_simulator, name='simulator')
