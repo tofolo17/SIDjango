86101470525b1e7f742e72316cc62b5032f11d73
gusta <valenciotofolo@usp.br>
2021-07-30 19:48:36 -0300

    Push para avaliação

--- ic/ic/settings.py
+++ ic/ic/settings.py
@@ -138,7 +138,7 @@ AUTHENTICATION_BACKENDS = [
 # Email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'disctest333@gmail.com'
-EMAIL_HOST_PASSWORD = 'discdisc111'
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
